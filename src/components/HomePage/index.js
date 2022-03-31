2e68a553c2c820077c7b60f78b7007532822291a
JordanMarcos <jordan.marcos.oclock@gmail.com>
2022-03-31 15:43:46 +0200

    home page done with arrows but the arrows are not working yet

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -9,6 +9,14 @@ function HomePage() {
       <div className="home-page-welcome-container">
         <p className="home-page-welcome" > Bienvenue... (déscription brève du site) Lorem ipsum dolor sit amet consectetur adipisicing elit. Error repudiandae praesentium accusamus ea ad, itaque doloremque aperiam corrupti voluptas rem inventore facilis saepe est neque, atque voluptatum ducimus numquam molestiae perspiciatis at repellat hic ex. Vitae numquam quibusdam quas tempora similique eligendi, molestiae iusto recusandae quasi, unde, enim qui ipsum?</p>
       </div>
+      <div className="container-arrow">
+       {/* <p>Déscendre</p> */}
+       <svg id="more-arrows">
+          <polygon class="arrow-top" points="37.6,27.9 1.8,1.3 3.3,0 37.6,25.3 71.9,0 73.7,1.3 "/>
+          <polygon class="arrow-middle" points="37.6,45.8 0.8,18.7 4.4,16.4 37.6,41.2 71.2,16.4 74.5,18.7 "/>
+          <polygon class="arrow-bottom" points="37.6,64 0,36.1 5.1,32.8 37.6,56.8 70.4,32.8 75.5,36.1 "/>
+       </svg>
+      </div>
     </div>
   );
 }
