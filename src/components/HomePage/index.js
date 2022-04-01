f1ff9c005ca171add3a43eb7f49cbf6b0d48883c
JordanMarcos <jordan.marcos.oclock@gmail.com>
2022-04-01 13:20:41 +0200

    updated arrows color and header blur

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -7,7 +7,7 @@ function HomePage() {
        <h1 className="home-page-title"> Starry Night </h1>
       </div>
       <div className="home-page-welcome-container">
-        <p className="home-page-welcome" > Bienvenue... (déscription brève du site) Lorem ipsum dolor sit amet consectetur adipisicing elit. Error repudiandae praesentium accusamus ea ad, itaque doloremque aperiam corrupti voluptas rem inventore facilis saepe est neque, atque voluptatum ducimus numquam molestiae perspiciatis at repellat hic ex. Vitae numquam quibusdam quas tempora similique eligendi, molestiae iusto recusandae quasi, unde, enim qui ipsum?</p>
+        <p className="home-page-welcome" > Bienvenue... (déscription brève du site) ex. Vitae numquam quibusdam quas tempora similique eligendi, molestiae iusto recusandae quasi, unde, enim qui ipsum?</p>
       </div>
       <div className="container-arrow">
        {/* <p>Déscendre</p> */}
