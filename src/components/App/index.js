855f0eda2e23e3ced6e6fc94b0ba42dae62fc26e
Linilolz <florent.dautin@gmail.com>
2022-04-08 18:56:54 +0200

    3 homepages sections

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,24 +1,26 @@
 // == Import
 import './styles.scss';
 import HomePage from '../HomePage';
-import TodayMyth from '../TodayMyth'
-//import { Routes, Route } from 'react-router-dom';
+import TodayMyth from '../TodayMyth';
+import Header from '../Header';
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 
 // == Composant App
 function App() {
   return (
-    <div className="app">
-      <HomePage /> 
-     {/*  <TodayMyth /> */}
+    <Router>    
+      <div className="app">
+        <HomePage />
+      
+        {/*On Gère les routes dans un second temps, d'abord créer chaque composant 
+        <Routes>
+          <Route path="/" element={<PageRepos />} />
+          <Route path="/" element={<PageFaq />} />
+          <Route path="*" element={(<div> 404 </div>)} />
+        </Routes> */}
+      </div> 
+    </Router>
 
-    
-      {/*On Gère les routes dans un second temps, d'abord créer chaque composant 
-      <Routes>
-        <Route path="/" element={<PageRepos />} />
-        <Route path="/" element={<PageFaq />} />
-        <Route path="*" element={(<div> 404 </div>)} />
-      </Routes> */}
-    </div>
   );
 }
 
--- src/components/TodayMyth/index.js
+++ src/components/TodayMyth/index.js
@@ -1,10 +1,12 @@
 import "./styles.scss";
 
+import ArrowDown from "../ArrowDown";
+
 function TodayMyth() {
   return (
-    <div className="myth-page-container">
+    <section id="Myth" className="myth-page-container Section">
       <div className="myth-page-title-container">
-        <h1 className="myth-page-title"> Mythe du jour </h1>
+        <h1 className="myth-page-title">Mythe du jour</h1>
       </div>
 
       <div className="myth-page-paragraph-container">
@@ -20,24 +22,8 @@ function TodayMyth() {
         </p>
       </div>
 
-      <div className="container-arrow">
-        {/* <p>Déscendre</p> */}
-        <svg id="more-arrows">
-          <polygon
-            class="arrow-top"
-            points="37.6,27.9 1.8,1.3 3.3,0 37.6,25.3 71.9,0 73.7,1.3 "
-          />
-          <polygon
-            class="arrow-middle"
-            points="37.6,45.8 0.8,18.7 4.4,16.4 37.6,41.2 71.2,16.4 74.5,18.7 "
-          />
-          <polygon
-            class="arrow-bottom"
-            points="37.6,64 0,36.1 5.1,32.8 37.6,56.8 70.4,32.8 75.5,36.1 "
-          />
-        </svg>
-      </div>
-    </div>
+      <ArrowDown href="#Map" />
+    </section>
   );
 }
 
