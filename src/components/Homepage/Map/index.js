d4c530de895e2e7af7070f39190aa91016c0f7d3
GauthierWebDev <gauthier@gauthierdaniels.fr>
2022-04-13 13:19:40 +0200

    Homepage Map refactored

--- src/components/Homepage/Map/index.js
+++ src/components/Homepage/Map/index.js
@@ -1,26 +1,13 @@
 import Form from './Form';
-// import Map from './Map';
-import ArrowDown from '../../ArrowDown';
-import './HomePageMap.scss';
+import './Map.scss';
 
-
-
-
-
-function Map() {
+const Map = () => {
   return (        
     <section id="Map" className="Map Section">
-      <div className="map-page-title-container">
-        <h1 className="map-page-title">Map</h1>
-      </div>
-      
-      <div className="map-page-welcome-container">
-        
-      </div>
-
+      <h1 className="Title">Map</h1>
       <Form href="#" />
-      <ArrowDown href="#Hero" />
     </section>
   );
-}
+};
+
 export default Map;
