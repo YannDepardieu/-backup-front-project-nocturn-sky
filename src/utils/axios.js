7b442c072948cf274bbc7f09693f1ad24a332b23
GauthierWebDev <gauthier@gauthierdaniels.fr>
2022-04-14 18:06:20 +0200

    Add constellations images and FillMurray for 404 images

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+export const baseURL = 'http://54.38.188.38:5000';
+export const ApiBaseURL = `${baseURL}/v1/api`;
+
 export default axios.create({
-  baseURL: 'http://54.38.188.38:5000/v1/api',
+  baseURL: ApiBaseURL,
 });
