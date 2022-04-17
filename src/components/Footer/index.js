ab90c211fb60851983c9a4164203963eff26ea4e
tatoberres <palm.sins@gmail.com>
2022-04-17 22:19:05 +0200

    favicon

---
+++ src/components/Footer/index.js
@@ -0,0 +1,8 @@
+import React, { useState, useContext, useEffect } from "react";
+import "./Footer.scss";
+
+function Footer() {
+  return <div className="Footer">'hola'</div>;
+}
+
+export default Footer;
