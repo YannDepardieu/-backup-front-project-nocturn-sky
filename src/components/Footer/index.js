e4a9f3590faa897f2f237e91d7d9f03ba8ef869c
tatoberres <palm.sins@gmail.com>
2022-04-21 09:59:13 +0200

    404 and footer details

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,34 +4,44 @@ import "./Footer.scss";
 function Footer() {
   return (
     <div className="Footer">
-      <div className="Footer-Section Description">
-        <h3 className="Footer-Section-Title">Notre quête</h3>
-        <p className="Footer-Section-Content">
-          Aider à partager des histoires au tour des étoiles pour apprécier les
-          qualités d'un beau ciel nocture à l'heure où la pollution lumineuse
-          devient un fléau.
-        </p>
+      <div className="Footer-Sections">
+        <div className="Footer-Sections-Section Description">
+          <h3 className="Footer-Sections-Section-Title">Notre quête</h3>
+          <p className="Footer-Sections-Section-Content">
+            Aider à partager des histoires au tour des étoiles pour apprécier
+            les qualités d'un beau ciel nocture à l'heure où la pollution
+            lumineuse devient un fléau.
+          </p>
+        </div>
+        <div className="Footer-Sections-Section Plan">
+          <h3 className="Footer-Sections-Section-Title">Plan du site</h3>
+          <ul>
+            <li>
+              <a className="Footer-Sections-Section-Link" href="/">
+                accueil
+              </a>
+            </li>
+            <li>
+              <a className="Footer-Sections-Section-Link" href="#Map">
+                carte interactive
+              </a>
+            </li>
+            <li>
+              <a
+                className="Footer-Sections-Section-Link"
+                href="/constellations"
+              >
+                constellations
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="Footer-Section Plan">
-        <h3 className="Footer-Section-Title">Plan du site</h3>
-        <ul>
-          <li>
-            <a className="Footer-Section-Link" href="/">
-              accueil
-            </a>
-          </li>
-          <li>
-            <a className="Footer-Section-Link" href="#Map">
-              carte interactive
-            </a>
-          </li>
-          <li>
-            <a className="Footer-Section-Link" href="/constellations">
-              constellations
-            </a>
-          </li>
-        </ul>
-      </div>
+      <p className="Footer-Bottom">
+        Cette application a été dévéloppée grâce à la librairie
+        <a href="https://github.com/ofrohn/d3-celestial"> D3-Celestial</a> d'
+        <a href="https://twitter.com/olaffrohn">Olaf Frohn</a>.
+      </p>
     </div>
   );
 }
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,101 @@
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import "./NotFound.scss";
+
 const NotFound = () => {
+  const particlesInit = async (main) => {
+    await loadFull(main);
+  };
+
+  const particlesLoaded = (container) => {
+    // console.log(container);
+  };
+
   return (
-    <>
-      404
-    </>
-  )
+    <main className="Main NotFound">
+      <h1 className="card404">404</h1>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          detectRetina: true,
+          background: {
+            // image: "url('../images/space.jpg')",
+            // image: "url('https://particles.js.org/images/background3.jpg')",
+            // position: "50% 50%",
+            // repeat: "no-repeat",
+            // size: "cover",
+          },
+          interactivity: {
+            detectsOn: "canvas",
+            events: {
+              onHover: {
+                enable: true,
+                mode: ["connect"],
+              },
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              resize: true,
+            },
+            modes: {
+              connect: {
+                distance: 800,
+                line_linked: {
+                  opacity: 0.7,
+                },
+                radius: 80,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: ["e5dff8", "9f98ca", "8479b9"],
+            },
+            links: {
+              enable: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 180,
+            },
+            opacity: {
+              value: 0.7,
+              random: true,
+              anim: {
+                enable: true,
+                speed: 1,
+                opacity_min: 0.1,
+                sync: false,
+              },
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 2,
+                color: "#000000",
+              },
+            },
+            size: {
+              value: { min: 0.5, max: 3.5 },
+              random: true,
+              anim: {
+                enable: true,
+                speed: 1,
+                size_min: 0.6,
+                sync: false,
+              },
+            },
+          },
+        }}
+      />
+    </main>
+  );
 };
 
 export default NotFound;
